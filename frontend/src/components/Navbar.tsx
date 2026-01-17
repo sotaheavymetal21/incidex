c6f0f0aa53bc390cf8b6efcd5021f0124b8a97b6
sotaheavymetal21 <euro246seeker@gmail.com>
2026-01-17 22:15:14 +0900

    refactor: ナビゲーションバーからテンプレートリンクを削除

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -37,12 +37,6 @@ export default function Navbar() {
       show: permissions.canViewIncidents,
     },
     {
-      name: 'テンプレート',
-      href: '/templates',
-      icon: 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z',
-      show: permissions.canViewTemplates,
-    },
-    {
       name: 'タグ管理',
       href: '/tags',
       icon: 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z',
